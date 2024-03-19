c7df1f628760d8cd49ca44a5693d7ac811e2789f
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-03-19 21:28:52 +0330

    Initial Commit

---
+++ UIKit Life Cycle in SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UIKit Life Cycle in SwiftUI
+//
+//  Created by Omid Heydarzadeh on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIKit Life Cycle in SwiftUI/UIKit_Life_Cycle_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKit_Life_Cycle_in_SwiftUIApp.swift
+//  UIKit Life Cycle in SwiftUI
+//
+//  Created by Omid Heydarzadeh on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct UIKit_Life_Cycle_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
